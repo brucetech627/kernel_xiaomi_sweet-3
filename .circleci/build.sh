81568559279c5690d1acf46c1060789e11e1a108
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-06-16 14:14:28 +0530

    kinda fix 2/2

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,77 +1,40 @@
-#!/usr/bin/env bash
+#!/bin/bash
 echo "Cloning dependencies"
+git clone --depth=1 -b main https://github.com/brucetech627/kernel_xiaomi_sweet-3 kernel
+cd kernel
 git clone --depth=1 -b master https://github.com/MASTERGUY/proton-clang clang
-git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b sweet AnyKernel
+git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b sweet AnyKernel3
 echo "Done"
-TANGGAL=$(date +"%F-%S")
-START=$(date +"%s")
 KERNEL_DIR=$(pwd)
-export CONFIG_PATH=$PWD/arch/arm64/configs/sweet_user_defconfig
-PATH="${KERNEL_DIR}/clang/bin:$PATH"
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+ANYKERNEL3_DIR="${KERNEL_DIR}/AnyKernel3"
+export PATH="${KERNEL_DIR}/clang/bin:${PATH}"
+export KBUILD_COMPILER_STRING="(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
 export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=brucetech
 export KBUILD_BUILD_HOST=circleci
-export KBUILD_BUILD_USER="brucetech627"
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
-        -d chat_id=$chat_id
-}
-# Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>• Predator-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note 10 Pro/Pro Max</b> (sweet)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
-}
-# Push kernel to channel
-function push() {
-    cd AnyKernel
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 10/Pro Max (sweet)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
-}
-# Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build throw an error(s)"
-    exit 1
-}
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 sweet_user_defconfig
+    make sweet_user_defconfig O=out
     make -j$(nproc --all) O=out \
-                    ARCH=arm64 \
-                    CC=clang \
-                    CLANG_TRIPLE=aarch64-linux-gnu- \
-                    CROSS_COMPILE=aarch64-linux-android- \
-                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+                      ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip
 
-    if ! [ -a "$IMAGE" ]; then
-        finerr
-        exit 1
-    fi
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+echo "**** Verify Image.gz-dtb & dtbo.img ****"
+ls $PWD/out/arch/arm64/boot/Image.gz-dtb
 }
 # Zipping
 function zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 Predator-Stormbreaker-sweet-${TANGGAL}.zip *
-    cd ..
+    cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+    cd $ANYKERNEL3_DIR || exit 1
+    zip -r9 Sweet-StormBreaker.zip *
+    curl https://bashupload.com/Sweet-StormBreaker.zip --data-binary @Sweet-StormBreaker.zip
 }
-sticker
-sendinfo
 compile
 zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
-push
-
