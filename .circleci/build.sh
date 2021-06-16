905ccd336aa9cc5bfd1fa63a2435d1a44a36f41d
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-06-16 15:06:52 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -13,6 +13,33 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER=brucetech
 export KBUILD_BUILD_HOST=circleci
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• Sweet-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Redmi Note 10 Pro/Max</b> (sweet/sweetin)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 13</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)."
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
 # Compile plox
 function compile() {
     make sweet_user_defconfig O=out
@@ -34,7 +61,10 @@ function zipping() {
     cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
     cd $ANYKERNEL3_DIR || exit 1
     zip -r9 Sweet-StormBreaker.zip *
-    curl https://bashupload.com/Sweet-StormBreaker.zip --data-binary @Sweet-StormBreaker.zip
+    curl --upload-file ./Sweet-StormBreaker.zip https://transfer.sh/Sweet-StormBreaker.zip
 }
+sendinfo
 compile
 zipping
+finerr
+push
